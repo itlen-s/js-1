b0d0303ee9cbd00abe7b4eed9f2991afa8909c4e
itlen-s <nabiullin-ildus@mail.ru>
2023-12-09 10:24:16 +0500

    32

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
+// var divs = document.querySelectorAll('div')
+// var link = document.querySelector('a')
 
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
- 
+// for (var i=0; i <divs.length; i++) {
+//     divs[i].addEventListener('click', function(event){
+//         event.stopPropagation()
+//         console.log(this.getAttribute('id'))
+//     })
+// }
+// link.addEventListener('click' , handleLinkClick)
+// function handleLinkClick (event) {
+//     event.preventDefault()
+
+//     var div = divs[0]
+//     // if (div.style.display === 'none') {
+//     //     div.style.display = 'flex'
+//     // } else { 
+//     //     div.style.display = 'none'
+//     // }
+//     div.style.display = div.style.display === 'none' ? 'flex' : 'none'
+//     console.log(div.style.display)
+// }
+
+////////////
+
+// var p = document.querySelectorAll('p')
+// for (var i=0; i<p.length;i++){
+//     p[i].addEventListener('click', function(event){
+//         event.target.style.color = 'blue'
+//     })
+// }
+
+document.getElementById('wrapper').addEventListener('click', function(event) {
+    var tagName = event.target.tagName.toLowerCase()
+    if (tagName === 'p') {
+        event.target.style.color = 'blue'
+    }
+    if (event.target.classList.contains('color')) {
+        event.target.style.color = 'red'
+    }
+})
+
